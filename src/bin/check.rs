e6df5d0a221eec91d5959e109b1423a0d37f68b6
mi2ebi <merrybot47@gmail.com>
2026-04-22 09:37:07 -0700

    add fonts->codepoints!

--- src/bin/check.rs
+++ src/bin/check.rs
@@ -117,13 +117,64 @@ fn process_block(block: &Block, font_map: &HashMap<u32, Vec<String>>) -> Value {
     })
 }
 
+fn invert_map(cp_map: &HashMap<u32, Vec<String>>) -> HashMap<String, Vec<u32>> {
+    let mut font_map: HashMap<String, Vec<u32>> = HashMap::new();
+    for (&cp, families) in cp_map {
+        for family in families {
+            font_map.entry(family.clone()).or_default().push(cp);
+        }
+    }
+    font_map
+}
+
+fn to_ranges(mut cps: Vec<u32>) -> Vec<(u32, u32)> {
+    cps.sort_unstable();
+    let mut ranges = Vec::new();
+    let mut start = cps[0];
+    let mut prev = cps[0];
+    for &cp in &cps[1..] {
+        if cp != prev + 1 {
+            ranges.push((start, prev));
+            start = cp;
+        }
+        prev = cp;
+    }
+    ranges.push((start, prev));
+    ranges
+}
+
+fn font_ranges_to_json(map: &HashMap<String, Vec<(u32, u32)>>) -> Value {
+    let mut fonts = map.iter().collect::<Vec<_>>();
+    fonts.sort_by_key(|(a, _)| a.to_lowercase());
+    let fonts = fonts
+        .into_iter()
+        .map(|(font, ranges)| {
+            let ranges_json = ranges
+                .iter()
+                .map(|(start, end)| {
+                    if start == end {
+                        json!(format!("{start:04x}"))
+                    } else {
+                        json!([format!("{start:04x}"), format!("{end:04x}")])
+                    }
+                })
+                .collect::<Vec<_>>();
+            json!({
+                "font": font,
+                "ranges": ranges_json
+            })
+        })
+        .collect::<Vec<_>>();
+    json!(fonts)
+}
+
 fn main() {
     let start_time = Instant::now();
-    let font_map = build_codepoint_map();
-    println!("font map built with {} codepoints", font_map.len());
+    let cp_map = build_codepoint_map();
+    println!("codepoint map built with {} codepoints", cp_map.len());
     let blocks = fetch_blocks();
     println!("there are {} blocks", blocks.len());
-    let mut data = blocks.par_iter().map(|c| process_block(c, &font_map)).collect::<Vec<_>>();
+    let mut data = blocks.par_iter().map(|c| process_block(c, &cp_map)).collect::<Vec<_>>();
     data.sort_by(|a, b| {
         let a_start = a["startdec"].as_u64().unwrap();
         let b_start = b["startdec"].as_u64().unwrap();
@@ -132,15 +183,26 @@ fn main() {
     for b in &mut data {
         b.as_object_mut().unwrap().remove("startdec");
     }
-    let json_data = serde_json::to_string_pretty(&data).unwrap() + "\n";
-    fs::write("data.json", &json_data).unwrap();
-    let min = serde_json::to_string(&data).unwrap() + "\n";
-    fs::write("data.min.json", &min).unwrap();
+    let bjson = serde_json::to_string_pretty(&data).unwrap() + "\n";
+    fs::write("blocks.json", &bjson).unwrap();
+    let bjson_min = serde_json::to_string(&data).unwrap() + "\n";
+    fs::write("blocks.min.json", &bjson_min).unwrap();
+    let font_ranges = invert_map(&cp_map)
+        .into_iter()
+        .map(|(font, cps)| {
+            let ranges = to_ranges(cps);
+            (font, ranges)
+        })
+        .collect();
+    println!("map inverted");
+    let fjson = serde_json::to_string_pretty(&font_ranges_to_json(&font_ranges)).unwrap();
+    fs::write("fonts.json", &fjson).unwrap();
     let elapsed = start_time.elapsed();
     println!("finished in {elapsed:?}");
     println!(
-        "{:.2} MiB pretty, {:.2} MiB minified",
-        json_data.len() as f64 / 1_048_576.,
-        min.len() as f64 / 1_048_576.
+        "blocks: {:5.2} MiB pretty, {:5.2} MiB minified\n fonts: {:5.2} MiB pretty",
+        bjson.len() as f64 / 1_048_576.,
+        bjson_min.len() as f64 / 1_048_576.,
+        fjson.len() as f64 / 1_048_576.
     );
 }
